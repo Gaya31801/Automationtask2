d10de660b2da76f4ce76a4fd57afbea02417a0e1
Gaya31801 <120543314+Gaya31801@users.noreply.github.com>
2022-12-30 10:39:55 +0530

    Create String3.java

---
+++ String3.java
@@ -0,0 +1,11 @@
+Task 3. Write a program to find the length of the string "refrigerator".
+
+
+package Task2.String;
+
+public class String3 {
+    public static void main(String[] args) {
+        String str = "refrigerator";
+        System.out.print(str.length());
+    }
+}
