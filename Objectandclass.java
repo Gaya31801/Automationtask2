41f407c02342ad4fa9ed51fd3492f41272a4097b
Gaya31801 <120543314+Gaya31801@users.noreply.github.com>
2022-12-30 10:44:53 +0530

    Create Objectandclass.java

---
+++ Objectandclass.java
@@ -0,0 +1,31 @@
+Task 1: Write a Java Program
+
+a) A class should consist of five global variables with value
+
+b) All variables should be printed in the main method through object creation.
+  
+  
+package Task2.object;
+
+public class objectandclass {
+
+    public static String a = "company";
+    public static String b = "Ionixxtech";
+    public static char c = 'I';
+    public static int d = 10;
+    public static float e = 1.2387f;
+
+    public static void main(String[] args) {
+
+        System.out.println(objectandclass.a);
+        System.out.println(objectandclass.b);
+        System.out.println(objectandclass.c);
+        System.out.println(objectandclass.d);
+        System.out.println(objectandclass.e);
+    }
+}
+
+
+
+
+
