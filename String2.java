dbb136a95cc2e7c84e12927ba110f58070c00f2a
Gaya31801 <120543314+Gaya31801@users.noreply.github.com>
2022-12-30 10:39:23 +0530

    Create String2.java

---
+++ String2.java
@@ -0,0 +1,13 @@
+Task 2. Write a program to input and display the sentence I love candies.
+
+
+package Task2.String;
+
+public class String2 {
+        public static void main(String[] args) {
+            String str="I love candies";
+            System.out.print(str);
+        }
+}
+
+
