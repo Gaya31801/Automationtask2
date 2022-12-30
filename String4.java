338342f249115c2785ceabd7f87879dfcb282c4d
Gaya31801 <120543314+Gaya31801@users.noreply.github.com>
2022-12-30 10:40:28 +0530

    Create String4.java

---
+++ String4.java
@@ -0,0 +1,16 @@
+Task 4. Write a program to check if the letter 'e' is present in the word 'Umbrella'.
+
+
+package Task2.String;
+
+public class String4 {
+    public static void main(String[] args) {
+        String str = "Umbrella";
+        if (str.contains("e")) {
+            System.out.print("True");
+        } else {
+            System.out.print("False");
+        }
+
+    }
+}
