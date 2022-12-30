6ec832ab0643c896984185428d61119d87a7d373
Gaya31801 <120543314+Gaya31801@users.noreply.github.com>
2022-12-30 10:38:50 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,13 @@
+Task 1. Write a program to print a string entered by user.
+
+package Task2.String;
+
+import java.util.Scanner;
+
+public class String1 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            System.out.print(str);
+        }
+}
